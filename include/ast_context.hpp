d0bf8c53a306c7e731f578f842d8824fe99d12ad
sarachehab <s.edu@chehab.co>
2024-02-24 00:42:59 +0000

    Added code for memory management within function: - allocating temporary registers to intermediate representations
    TODO:
    - test
    - write code for overflowing of registers in memory

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -6,7 +6,33 @@
 // compiled (e.g. function scope and variable names).
 class Context
 {
-    /* TODO decide what goes inside here */
+
+private:
+
+    int used_registers[32] = {
+        1,                              // zero
+        1,                              // return address ra
+        1,                              // stack pointer sp
+        1,                              // global pointer gp
+        1,                              // thread pointer tp
+        0, 0, 0,                        // temporaries t0-2
+        1,                              // saved register s0 / frame pointer fp
+        1,                              // saved register s1
+        1, 1,                           // function arguments and return values a0-1
+        1, 1, 1, 1, 1, 1,               // function arguments a2-7
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // saved register s2-11
+        0, 0, 0, 1                      // saved registers t3-6
+    };
+
+public:
+
+    ~ Context(){}
+    Context(){}
+
+    void useRegister(int i);
+    void freeUpRegister(int i);
+    int allocateRegister();
+    
 };
 
 #endif
--- src/ast_context.cpp
+++ src/ast_context.cpp
@@ -1,1 +1,26 @@
 #include "ast_context.hpp"
+#include <iostream>
+
+
+void Context::useRegister(int i){
+    used_registers[i] = 1;
+}
+
+void Context::freeUpRegister(int i){
+    used_registers[i] = 0;
+}
+
+int Context::allocateRegister(){
+
+    // find unused temporary register
+    for (int i = 5; i < 32; i++){
+        if (!used_registers[i]){
+            useRegister(i);
+            return i;
+        }
+    }
+
+    // TODO solution if all temporary registers are used
+    throw std::runtime_error("All temporary registers used up! Check ast_context.cpp");
+
+}
